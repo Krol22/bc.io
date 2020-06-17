296ba5b1b3c3311554e6d775bca84160b2e33659
Karol Torun <krol22ee@gmail.com>
2020-06-17 20:40:45 +0200

    Fixing index.js protocol

--- src/client/index.js
+++ src/client/index.js
@@ -37,6 +37,9 @@ function connect() {
     return;
   } 
 
+
+  const protocol = process.env.SERVER==='localhost:3000' ? 'http' : 'https';
+
   const address = `http://${process.env.SERVER}/?room=${roomId}`;
 
   socket = io(address);
