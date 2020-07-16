058fb6a94fbf415a1a05a02a71ed90adbd1d53f6
Karol Torun <krol22ee@gmail.com>
2020-07-16 15:25:05 +0200

    Added jsdoc to networkManager

--- src/client/networkManager.js
+++ src/client/networkManager.js
@@ -1,6 +1,14 @@
 import networkEvents from '../common/constants/networkEvents';
 
+/**
+ * NetworkManager class responsible for communication between client <-> server
+*/
 export default class NetworkManager {
+  /**
+   * Starts listen on server events based on networkEvents object.
+   * @constructor
+   * @param {object} socket - player socket connected to game server,
+   */
   constructor(socket) {
     this.connected = false;
     this.socket = socket;
@@ -14,16 +22,55 @@ export default class NetworkManager {
         this.events[event] = [];
 
         socket.on(event, (args) => {
-          this.onNetworkEvent(event, args);
+          this._onNetworkEvent(event, args);
         });
       });
   }
 
-  sendClientEvent(eventName, event) {
-    this.socket.emit(eventName, event);
+  /**
+    * Send event from client to server with payload.
+    * @param {string} eventName - event name
+    * @param {object} payload - event payload 
+    */
+  sendClientEvent(eventName, payload) {
+    this.socket.emit(eventName, payload);
   }
 
-  onNetworkEvent(eventName, args) {
+  /** 
+    * Server event listener callback function
+    * @callback eventCallback
+    * @param {Object} payload - event payload
+    */
+
+  /**
+    * Add listener for server event
+    * @param {string} eventName - event name
+    * @param {eventCallback} callback - event callback
+    */
+  addEventListener(eventName, callback) {
+    this.events[eventName].push(callback);
+  }
+
+  /**
+    * Remove event listener
+    * @param {string} eventName - event name
+    * @param {eventCallback} callback - event callback referenct
+    */
+  removeEventListener(eventName, callback) {
+    const index = this.events[eventName].indexOf(callback); 
+
+    this.events[eventName].splice(index, 1);
+  }
+
+  /**
+    * Remove all event listeners for server event
+    * @param {string} eventName - event name
+    */
+  removeAllEventListeners(eventName) {
+    this.events[eventName] = [];
+  }
+
+  _onNetworkEvent(eventName, args) {
     this
       .events[eventName]
       .forEach(eventCallback => {
@@ -31,17 +78,4 @@ export default class NetworkManager {
       });
   }
 
-  addEventListener(eventName, callback) {
-    this.events[eventName].push(callback);
-  }
-
-  removeEventListener(eventName, callback) {
-    const index = this.events[eventName].indexOf(callback); 
-
-    this.events[eventName].splice(index, 1);
-  }
-
-  removeAllEventListeners(eventName) {
-    this.events[eventName] = [];
-  }
 }
